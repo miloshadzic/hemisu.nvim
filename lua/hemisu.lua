3c1d1570b8fb58c454f136feb43cd128c29de790
Miloš Hadžić <milos@rightfold.io>
2021-05-27 15:14:25 +0200

    Test color names

--- lua/hemisu.lua
+++ lua/hemisu.lua
@@ -1,11 +1,11 @@
 local Color, colors, Group, groups, styles = require("colorbuddy").setup()
 
-Color.new('white', '#EEEEEE')
-Color.new('black', '#000000')
+Color.new('White', '#EEEEEE')
+Color.new('Black', '#000000')
 
-Color.new('red',       '#63001C')
-Color.new('pink',      '#FFAFAF')
-Color.new('green',     '#5F5F00')
+Color.new('Red',       '#63001C')
+Color.new('Pink',      '#FFAFAF')
+Color.new('Green',     '#5F5F00')
 Color.new('yellow',    '#503D15')
 Color.new('blue',      '#005F87')
 Color.new('aqua',      '#CBE4EE')
@@ -15,5 +15,5 @@ Color.new('violet',    '#FF0055')
 Color.new('orange',    '#ECE1C8')
 Color.new('brown',     '#503D15')
 
-Color.new('background', '#000000')
+Color.new('background', colors.Black)
 Color.new('fg', '#EEEEEE')
