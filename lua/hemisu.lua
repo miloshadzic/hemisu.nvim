73d910ce82877e08a177b066034b499613fe79eb
Miloš Hadžić <milos@rightfold.io>
2021-05-27 12:55:37 +0200

    Initial commit

---
+++ lua/hemisu.lua
@@ -0,0 +1,18 @@
+require('colorbuddy').setup()
+
+local Color = require('colorbuddy').Color
+
+Color.new('white', '#ffffff')
+Color.new('black', '#EEEEEE')
+
+Color.new('red',       '#63001C')
+Color.new('pink',      '#FFAFAF')
+Color.new('green',     '#5F5F00')
+Color.new('yellow',    '#503D15')
+Color.new('blue',      '#005F87')
+Color.new('aqua',      '#CBE4EE')
+Color.new('cyan',      '#9FD3E6')
+Color.new('purple',    '#538192')
+Color.new('violet',    '#FF0055')
+Color.new('orange',    '#ECE1C8')
+Color.new('brown',     '#503D15')
